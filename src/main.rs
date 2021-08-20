93cea11de67d2892a2499979e05f5868d4403910
Osiris9999 <rishikeshmahi@gmail.com>
2021-08-20 15:24:59 +0800

    revert string

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod linux;
 
+fn Revert(s: String) -> String {
+	let reverted_string = s.chars().rev().collect::<String>();	
+	return reverted_string 
+	}
+
 fn StrToInt(string_integer: String) -> u32 {
 	return string_integer.parse::<u32>().unwrap()
 	}
