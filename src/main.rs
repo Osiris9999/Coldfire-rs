ba3a5935130d1ceed8b43bb1956d9fba2441c101
Osiris9999 <rishikeshmahi@gmail.com>
2021-08-20 11:55:30 +0800

    add string convertions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 mod linux;
 
+fn StrToInt(string_integer: String) -> u32 {
+	return string_integer.parse::<u32>().unwrap()
+	}
+
+fn IntToStr(i: u32) -> String {
+	return i.to_string()
+	}
+
 fn main() {
     println!("Hello, world!");
 }
