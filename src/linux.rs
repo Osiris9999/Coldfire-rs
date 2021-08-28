b40db55798cbfdead27a11e20cb233454b85e13d
Osiris9999 <rishikeshmahi@gmail.com>
2021-08-28 19:51:32 +0800

    Removed dead code

--- src/linux.rs
+++ src/linux.rs
@@ -1,39 +1,23 @@
-use std::process::{Command, Output, ExitStatus};
-use std::io::Error;
-use nix::unistd::getuid;
+use std::process::Command;
 
-fn killProcByPID(pid:i32) {
-	let p = pid.to_string();
-	let cmd = "kill -9".to_owned() + &p;
-	let err = cmdOut(cmd);	
+fn isRoot() -> bool {
+    let (u, err) = cmdOut("whoami");
+    if u == "root" {
+        true
+    } else {
+        false
+    }
 }
 
-//fn info() -> String {
-//	let user = Command::new("whoami").output().expect("Failed to execute command");
-//	let user = match user {
-//		Ok(Result) => output,
-//		Err(error) => println!("{}",error),
-//	}; 
-
-//}
-
-fn isRoot() -> String  {
-	 let u = getuid();
-         if u32::from(u) == 0 {
-                 "root".to_string()
-                }
-        else {
-                 "not root".to_string()
-                }
-
-	
-
-	}
-
-fn cmdOut(command: String)  -> ExitStatus {
-	let output = Command::new("bash").arg("-c").arg(command).status().expect("Failed to execute command");
-	 output
-	}
-
+fn cmdOut(command: &str) -> (String, String) {
+    let output = Command::new("bash")
+        .arg("-c")
+        .arg(command)
+        .output()
+        .expect("Failed to execute command");
+    let out = String::from_utf8_lossy(&output.stdout).replace("\n", "");
+    let err = String::from_utf8_lossy(&output.stderr).replace("\n", "");
+    (out, err)
+}
 
 fn main() {}
